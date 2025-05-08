46392f30e4f042f46c38e4e5cf4950e0e8c644c9
7vibex <160543187+7vibex@users.noreply.github.com>
2025-05-09 00:23:14 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,24 @@
+
+function nextPage() {
+    window.location.href = "yes.html";
+}
+
+
+function moveButton() {
+    const button = document.getElementById("noButton");
+
+   
+    const randomX = Math.random() * (window.innerWidth - button.clientWidth);
+    const randomY = Math.random() * (window.innerHeight - button.clientHeight);
+  
+    button.style.transition = "all 0.2s ease";
+    
+
+    const randomAngle = Math.random() * 360;
+
+  
+    button.style.position = "absolute";
+    button.style.left = `${randomX}px`;
+    button.style.top = `${randomY}px`;
+    button.style.transform = `rotate(${randomAngle}deg)`;
+}
